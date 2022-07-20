cb759ec707e17c659292c0fa657b6234be9452d6
malyadri beegala <malyadri.beegala@gmail.com>
2022-07-20 16:35:35 +0530

    adding new examples

---
+++ sqrt_math.py
@@ -0,0 +1,10 @@
+# This program read a numbers prints square root of that number with math module
+import math
+
+number = float(input('Enter a number: '))
+
+# sqrt of a number
+sqrt = math.sqrt(number)
+
+# Display the sqrt
+print('square root of ',number, " is ", sqrt)
